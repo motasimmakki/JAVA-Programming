b51d319ec553f6d006c6094c6dc1aaaa0847d3d3
Motasim <44056349+motasimmakki@users.noreply.github.com>
2021-06-13 22:36:45 +0530

    Initial Commit (Using Stack)
    This is a very popular problem in which we have been given a string containing the Infix Expression and we have to convert it into the Postfix Expression.
    .
    Time Complexity: O(n), Where 'n' is the length of the given Infix string.
    Auxiliary Space: O(n), An extra stack has been used to solve it.

---
+++ InfixToPostfix.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class InfixToPostfix{
+	public int getPriority(char token){
+		if(token=='+'||token=='-')
+			return 1;
+		if(token=='*'||token=='/'||token=='%')
+			return 2;
+		return 3;
+	}
+	public boolean isOperator(char token){
+		if(token=='+'||token=='-'||token=='*'||token=='/'||token=='%'||token=='^'||token=='$' ||token=='('||token==')')
+			return true;
+		return false;
+	}
+	public void getPostfix(String infix){
+		Stack<Character> stack=new Stack<Character>();
+		ArrayList<Character> arrayList=new ArrayList<Character>();
+		stack.push('(');
+		infix=infix+')';
+		for(int i=0;i<infix.length();i++){
+			if(isOperator(infix.charAt(i))){
+				if((stack.isEmpty()||infix.charAt(i)=='('||(getPriority(stack.peek())<getPriority(infix.charAt(i))))&&(infix.charAt(i)!=')')){
+					stack.push(infix.charAt(i));
+				}
+				else{
+					if(infix.charAt(i)!=')'){
+						while((!(getPriority(stack.peek())<getPriority(infix.charAt(i))))&&((stack.peek()!='('))){
+							arrayList.add(stack.pop());
+						}
+						stack.push(infix.charAt(i));
+					}
+				}
+				if(infix.charAt(i)==')'){
+					while((!stack.isEmpty())&&stack.peek()!='('){
+						arrayList.add(stack.pop());
+					}
+					if(stack.peek()=='(')
+						stack.pop();
+				}
+			}
+			else{
+				arrayList.add(infix.charAt(i));
+			}
+		}		
+		while(!stack.isEmpty()&&stack.peek()!='('){
+			arrayList.add(stack.pop());
+		}
+		System.out.println("Postfix Is : "+arrayList);
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		InfixToPostfix infixToPostfix=new InfixToPostfix();
+		System.out.println("Enter The Infix To Convert It Into Postfix : ");
+		infixToPostfix.getPostfix(scanner.nextLine());
+	}
+}
